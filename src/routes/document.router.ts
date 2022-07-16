560ee1d7dc1fae89df7aa51e083dddbe0eec26f7
axel valles <axel.valles@activeone.co>
2022-07-16 11:17:49 -0500

    test get

--- src/routes/document.router.ts
+++ src/routes/document.router.ts
@@ -13,10 +13,8 @@ export default class DocumentRouter {
     this.router.post('/create-document', (req, res) => this.documentController.sendEmail(req, res))
     this.router.get('/', (req, res) => {
       res.json({
-        host: process.env.MAILER_HOST || 'host_not_configured',
-        port: Number(process.env.MAILER_PORT) || 465,
-        user: process.env.MAILER_USER || 'email_acount_not_configured',
-        pass: process.env.MAILER_PASS || 'pass_not_configured'
+        ok: true,
+        msg: 'test'
       })
     })
   }
